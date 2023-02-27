3d2ebcfdf2d80005c92004c4467cbcf225d77635
jinxianwie <2948950250@qq.com>
2023-02-27 11:12:02 +0800

    good lr for mask-rcnn

--- configs/all_changed/mask-rcnn_r50_fpn_1x_coco.py
+++ configs/all_changed/mask-rcnn_r50_fpn_1x_coco.py
@@ -14,11 +14,11 @@ train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=100, val_interval=1)
 
 optim_wrapper = dict(
     type='OptimWrapper',
-    optimizer=dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001))
+    optimizer=dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001))
 param_scheduler = dict(
     _delete_=True,
-    type='CosineAnnealingLR', by_epoch=True, T_max=100, eta_min=1e-6)
+    type='CosineAnnealingLR', by_epoch=False, T_max=100, eta_min=1e-6)
 
 load_from = 'https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_fpn_mstrain-poly_3x_coco/mask_rcnn_r50_fpn_mstrain-poly_3x_coco_20210524_201154-21b550bb.pth'
 
-work_dir = '/root/autodl-tmp/mmdet_workdir/mask-rcnn'
+work_dir = '/root/autodl-tmp/mmdet_workdir/mask-rcnn/temp2'
